bce37a0c64be24d648c88c1cb2b687de28b2df78
Alex Butler <alex@alex-j-butler.com>
2023-04-09 16:01:52 +1000

    Updated restore.sh to drop schema prior to restore.

--- restore-image/restore.sh
+++ restore-image/restore.sh
@@ -4,9 +4,13 @@ TEMP_FOLDER="/mysqlsh/temp"
 cd $TEMP_FOLDER
 restic dump $SNAPSHOT_ID $SNAPSHOT_PATH | tar xvf -
 
+# Drop the sql schema we're restoring.
+mysqlsh -h $SQL_HOST -p$SQL_PASS -u $SQL_USER --js --recreate-schema -D $SQL_DATABASE
+
 # /mysqlsh/temp/mysqlsh/backup
 mysqlsh -h $SQL_HOST -p$SQL_PASS -u $SQL_USER --js <<EOF
   util.loadDump('/mysqlsh/temp/mysqlsh/backup',
                 { threads: 4,
-				  showProgress: true})
+				  showProgress: true,
+				  includeSchemas: ["$SQL_DATABASE"]})
 EOF
